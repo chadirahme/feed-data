656228a166628d6b75098353b84deb474a20cdfd
Chadi Rahme <eng.chadi@gmail.com>
2024-11-26 22:46:14 -0500

    first commit

---
+++ src/main/java/com/tacticals/feeddata/FeedDataApplication.java
@@ -0,0 +1,13 @@
+package com.tacticals.feeddata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tacticals/feeddata/FeedDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacticals.feeddata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
