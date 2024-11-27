61a877b99ad382943f27a4651c496103c90e722a
Chadi Rahme <eng.chadi@gmail.com>
2024-11-26 23:54:01 -0500

    add deploy step

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Deployed in 1:"
+start=$(date +"%s")
+
+ssh -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
+docker pull chadirahme/feed-data:latest
+
+CONTAINER_NAME=feed-data
+if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
+    if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+        echo "Container is running -> stopping it..."
+        docker stop $CONTAINER_NAME;
+    fi
+fi
+
+docker run -d --rm -p 8001:8001 --name $CONTAINER_NAME chadirahme/feed-data:latest
+
+exit
+ENDSSH
+
+if [ $? -eq 0 ]; then
+  exit 0
+else
+  exit 1
+fi
+
+end=$(date +"%s")
+
+diff=$(($end - $start))
+
+echo "Deployed in : ${diff}s"
